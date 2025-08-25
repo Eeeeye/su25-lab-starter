830f5e233baf3eb83315547566412f2f8fd8066e
Eeeeye <2154339681@qq.com>
2025-08-26 02:05:40 +0800

    lab02

--- lab02/ex8_double_pointers.c
+++ lab02/ex8_double_pointers.c
@@ -28,13 +28,12 @@ int main() {
 
   free(student1_ptr);
 
-  student_t *student2_ptr = malloc(sizeof(student_t));
+  student_t *student2_ptr = NULL;
 
   create_student_2(&student2_ptr, 6);
 
   printf("Student 2's ID: %d\n", student2_ptr->id);
 
   free(student2_ptr);
-
   return 0;
 }
